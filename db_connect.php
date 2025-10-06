a20b958e912922050dd630e8f94f47c85c78db4e
Hyper7711 <shindeyash054@gmail.com>
2025-10-06 21:14:06 +0530

    slove connetion problem...

--- db_connect.php
+++ db_connect.php
@@ -1,14 +1,15 @@
 <?php
 // db_connect.php
 
-$host = "localhost";   // Database host
+$host = "localhost";       // Database host
 $dbname = "bus_management"; // Your database name
-$username = "root";    // Default XAMPP MySQL user
-$password = "";        // Default XAMPP MySQL password is empty
+$username = "root";        // Default XAMPP MySQL user
+$password = "";            // Default XAMPP MySQL password is empty
+$port = 3307;              // MySQL port
 
 try {
-    // Create PDO connection
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    // Create PDO connection with port
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
 
     // Set error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
